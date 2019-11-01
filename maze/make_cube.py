9c1437f4702f08b3c1aa909d11d46e357841364b
Nick Moore <nick@zoic.org>
2019-11-01 13:14:48 +1100

    add cube-making script

---
+++ maze/make_cube.py
@@ -0,0 +1,100 @@
+import random
+
+def make_mesh(nx, ny, nz):
+
+    def number(x,y,z):
+        return z * ny * nx + y * nx + x
+
+    mesh = dict()
+
+    for x in range(0, nx):
+        for y in range(0, ny):
+            for z in range(0, nz):
+                nn = set()
+                if x > 0: nn.add(number(x-1,y,z))
+                if x < nx-1: nn.add(number(x+1,y,z))
+                if y > 0: nn.add(number(x,y-1,z))
+                if y < ny-1: nn.add(number(x,y+1,z))
+                if z > 0: nn.add(number(x,y,z-1))
+                if z < nz-1: nn.add(number(x,y,z+1))
+                mesh[number(x,y,z)] = nn
+
+    return mesh
+
+def is_connected(mesh):
+    mesh_keys = set(mesh.keys())
+    new_keys = set([list(mesh_keys)[0]])
+    found_keys = set()
+
+    while len(new_keys) and len(found_keys) < len(mesh_keys):
+        new_key = new_keys.pop()
+        new_keys.update(mesh[new_key].difference(found_keys))
+        found_keys.update(mesh[new_key])
+
+    return len(found_keys) == len(mesh_keys)
+
+def perturb_mesh(mesh):
+    key1 = random.choice(list(mesh.keys()))
+    key2 = random.choice(list(mesh[key1]))
+    new = mesh.copy()
+    new[key1] = mesh[key1].copy()
+    new[key1].remove(key2)
+    new[key2] = mesh[key2].copy()
+    new[key2].remove(key1)
+    return new
+
+def mesh_to_pairs(mesh):
+    for key1 in mesh.keys():
+        for key2 in mesh[key1]:
+            if key2 > key1:
+                yield key1, key2
+
+dim = 5
+
+mesh = make_mesh(dim,dim,dim)
+count = 1000
+while count:
+    new = perturb_mesh(mesh)
+    if is_connected(new):
+        mesh = new
+        count = 1000
+    else:
+        count -= 1
+
+def find_xyz(n):
+   return (n // (dim * dim)) * 10, ((n // dim) % dim) * 10, (n % dim) * 10 
+
+#print("""
+#// http://forum.openscad.org/Rods-between-3D-points-tp13104p13115.html
+#module rod(p1,p2,r){ // draw ray between 2 specified points
+#  translate((p1+p2)/2)
+#    rotate([-acos((p2[2]-p1[2]) / norm(p1-p2)),0,
+#            -atan2(p2[0]-p1[0],p2[1]-p1[1])])
+#       cylinder(r=r, h=norm(p1-p2), center = true);
+#}
+#$fn=16;
+#""")
+
+#print("""
+#difference() {
+#    translate([-7,-7,-7]) cube([%d,%d,%d]);
+#""" % (dim*10+4, dim*10+4, dim*10+4))
+
+print("""
+difference() {
+    hull() {
+""")
+
+for k1 in mesh.keys():
+    x1,y1,z1 = find_xyz(k1)
+    print("        translate([%d,%d,%d]) sphere(r=6); // %d" % (x1,y1,z1,k1))
+
+print("    }")
+
+for k1, k2 in mesh_to_pairs(mesh):
+    x1,y1,z1 = find_xyz(k1)
+    x2,y2,z2 = find_xyz(k2)
+    #print("rod([%d,%d,%d],[%d,%d,%d],4.5); // %d -- %d" % (x1,y1,z1,x2,y2,z2,k1,k2))
+    print("    hull() { translate([%d,%d,%d]) sphere(r=4.5); translate([%d,%d,%d]) sphere(r=4.5); } // %d -- %d" % (x1,y1,z1,x2,y2,z2,k1,k2))
+
+print("}")
